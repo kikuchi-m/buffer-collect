2e05a39e826ea771fc18741f8df9b22ee1ea5bb7
Motoki Kikuchi <bp_jda.mk@live.jp>
2014-01-15 01:45:06 +0900

    Fix prompting.

--- buffer-collect.el
+++ buffer-collect.el
@@ -13,19 +13,23 @@
                    "\\|"))
     (mapconcat 'identity bc:type-list " ")))
 
+(defun bc:type-list-str-prompt ()
+  (let ((default-str (bc:type-list-to-string)))
+    (read-string "Buffer type: " default-str nil default-str)))
+
 (defun bc:set-type-list (type-str)
   "TYPE-STR is a string of extensions or name patterns
 separated white space. (ex. \".el .h .c foo bar\")
+TYPE-STR is needed non empty string.
 If a pattern contains white space, escape it with backslash."
-  (interactive (list (let ((default-str (bc:type-list-to-string)))
-                       (read-string "Buffer type: " default-str nil default-str))))
-  (setq bc:type-list (split-string type-str)))
+  (if (or (not type-str) (string-equal type-str ""))
+      (error "Cannot set empty.")
+    (setq bc:type-list (split-string type-str))))
 
-(defun bc:collect-buffers (&optional type-str)
+(defun bc:collect-buffers (&optional type-str interactively)
   "Parameter TYPE-STR is same as `bc:set-type-list'."
-  (let ((p (progn
-             (when type-str (setq bc:type-list (split-string type-str)))
-             (bc:type-list-to-string t))))
+  (bc:set-type-list (if interactively (bc:type-list-str-prompt) type-str))
+  (let ((p (bc:type-list-to-string t)))
     (reduce (lambda (acc buf)
               (if (string-match p (buffer-name buf))
                   (cons buf acc)
